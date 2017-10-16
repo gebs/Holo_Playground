2595ed9e13ae5aeadc4a11c5a625d1cf836a6093
gebs <adrian.gebs@stud.hslu.ch>
2017-10-16 22:22:41 +0200

    Versuäch Nummer 3... Cha ja nor besser wärdä

---
+++ Gebs/Sprint1v3/Sprint1v3/Assets/Scripts/BoardPlacer.cs
@@ -0,0 +1,59 @@
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardPlacer : MonoBehaviour, IInputClickHandler, ISourceStateHandler
+{
+    private uint trackedHandsCount = 0;
+    private bool placement = true;
+
+    public GameObject gameObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (placement)
+        {
+            var headPosition = Camera.main.transform.position;
+            var gazeDirection = Camera.main.transform.forward;
+
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo, 30.0f, SpatialMappingManager.Instance.LayerMask))
+            {
+                gameObject.transform.parent.position = hitInfo.point;
+
+                Quaternion toQuat = Camera.main.transform.localRotation;
+                toQuat.x = 0;
+                toQuat.y = 0;
+                gameObject.transform.parent.rotation = toQuat;
+            }
+        }
+
+    }
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        placement = !placement;
+        SpatialMappingManager.Instance.DrawVisualMeshes = placement;
+
+    }
+
+    public void OnSourceDetected(SourceStateEventData eventData)
+    {
+        trackedHandsCount++;
+    }
+
+    public void OnSourceLost(SourceStateEventData eventData)
+    {
+        trackedHandsCount--;
+    }
+}
